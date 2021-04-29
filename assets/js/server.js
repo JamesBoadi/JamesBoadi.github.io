ea71c51db75e903045729547c736dbcd355ca38e
bob <39907481+JamesBoadi@users.noreply.github.com>
2021-04-29 10:33:58 +0100

    Update

--- assets/js/server.js
+++ assets/js/server.js
@@ -12,7 +12,7 @@ server.listen(8001, function () {
 });
 
 server.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8000/");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8001/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
